ba0d3ac3681a34bf603ac6fdadca0b57feb2b8ec
Emil Knabe <emil.knabe@volvocars.com>
2019-06-21 07:33:35 +0200

    Added initial functionality to RoadManagerDLL

--- EnvironmentSimulator/RoadManagerDLL/roadmanagerdll.cpp
+++ EnvironmentSimulator/RoadManagerDLL/roadmanagerdll.cpp
@@ -12,13 +12,298 @@
 
 #include "roadmanagerdll.hpp"
 #include "roadmanager.hpp"
+#include "CommonMini.hpp"
 
 using namespace roadmanager;
+
+static roadmanager::OpenDrive *odrManager = 0;
+static std::vector<Position> position;
+
+static int GetSteeringTarget(int index, float lookahead_distance, double *pos_local, double *pos_global, double *angle, double *curvature)
+{
+	if (odrManager == 0)
+	{
+		return -1;
+	}
+
+	if (index >= position.size())
+	{
+		LOG("Object %d not available, only %d registered", index, position.size());
+		return -1;
+	}
+
+	position[index].GetSteeringTargetPos(lookahead_distance, pos_local, pos_global, angle, curvature);
+
+	return 0;
+}
 
 extern "C"
 {
 	RM_DLL_API int RM_Init(const char *odrFilename)
 	{
+		if (!roadmanager::Position::LoadOpenDrive(odrFilename))
+		{
+			printf("Failed to load ODR %s\n", odrFilename);
+			return -1;
+		}
+		odrManager = roadmanager::Position::GetOpenDrive();
+
+		return 0;
+	}
+
+	RM_DLL_API int RM_Close()
+	{
+		position.clear();
+		return 0;
+	}
+	
+	RM_DLL_API int RM_CreatePosition()
+	{
+		roadmanager::Position newPosition;
+		position.push_back(newPosition);
+		return (int)(position.size() - 1);  // return index of newly created 
+	}
+
+	RM_DLL_API int RM_GetNumberOfRoads()
+	{
+		if (odrManager)
+		{
+			return odrManager->GetNumOfRoads();
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	RM_DLL_API int RM_GetIdOfRoadFromIndex(int index)
+	{
+		if (odrManager)
+		{
+			return odrManager->GetRoadByIdx(index)->GetId();
+		}
+		else
+		{
+			return -1;
+		}
+	}
+
+	RM_DLL_API float RM_GetRoadLength(int id)
+	{
+		if (odrManager)
+		{
+			return (float)(odrManager->GetRoadById(id)->GetLength());
+		}
+		else
+		{
+			return 0.0f;
+		}
+	}
+
+	RM_DLL_API int RM_GetRoadNumberOfLanes(int roadId, float s)
+	{
+		int numberOfDrivableLanes = 0;
+
+		if (odrManager)
+		{
+			roadmanager::Road *road = odrManager->GetRoadById(roadId);
+
+			// Consider only drivable lanes
+			roadmanager::LaneSection *laneSection = road->GetLaneSectionByS(s);
+			for (size_t i = 0; i < laneSection->GetNumberOfLanes(); i++)
+			{
+				if (laneSection->GetLaneByIdx((int)i)->IsDriving())
+				{
+					numberOfDrivableLanes++;
+				}
+			}
+		}
+
+		return numberOfDrivableLanes;
+	}
+
+	RM_DLL_API int RM_GetLaneIdByIndex(int roadId, int laneIndex, float s)
+	{
+		int numberOfDrivableLanes = 0;
+
+		if (odrManager)
+		{
+			roadmanager::Road *road = odrManager->GetRoadById(roadId);
+
+			// Consider only drivable lanes
+			roadmanager::LaneSection *laneSection = road->GetLaneSectionByS(s);
+			for (size_t i = 0; i < laneSection->GetNumberOfLanes(); i++)
+			{
+				if (laneSection->GetLaneByIdx((int)i)->IsDriving())
+				{
+					if (numberOfDrivableLanes == laneIndex)
+					{
+						return laneSection->GetLaneByIdx((int)i)->GetId();
+					}
+					else
+					{
+						numberOfDrivableLanes++;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+		
+	RM_DLL_API int RM_SetLanePosition(int handle, int roadId, int laneId, int laneOffset, float s)
+	{
+		if (odrManager == 0 || handle >= position.size())
+		{
+			return -1;
+		}
+		else
+		{
+			roadmanager::Position *pos = &position[handle];
+			pos->SetLanePos(roadId, laneId, s, laneOffset);
+		}
+
+		return 0;
+	}
+
+	RM_DLL_API int RM_SetWorldPosition(int handle, float x, float y, float z, float h, float p, float r)
+	{
+		if (odrManager == 0 || handle >= position.size())
+		{
+			return -1;
+		}
+		else
+		{
+			roadmanager::Position *pos = &position[handle];
+			pos->SetInertiaPos(x, y, z, h, p, r);
+		}
+
+		return 0;
+	}
+
+	RM_DLL_API int RM_SetS(int handle, float s)
+	{
+		if (odrManager == 0 || handle >= position.size())
+		{
+			return -1;
+		}
+		else
+		{
+			roadmanager::Position *pos = &position[handle];
+			pos->SetLanePos(pos->GetTrackId(), pos->GetLaneId(), s, pos->GetOffset());
+		}
+
+		return 0;
+	}
+
+	RM_DLL_API int RM_PositionMoveForward(int handle, float dist)
+	{
+		if (odrManager == 0 || handle >= position.size())
+		{
+			return -1;
+		}
+		else
+		{
+			roadmanager::Position *pos = &position[handle];
+			return(pos->MoveAlongS(dist));
+		}
+	}
+
+	RM_DLL_API int RM_GetPositionData(int handle, PositionData *data)
+	{
+		if (odrManager == 0 || handle >= position.size())
+		{
+			return -1;
+		}
+		else
+		{
+			data->x = (float)position[handle].GetX();
+			data->y = (float)position[handle].GetY();
+			data->z = (float)position[handle].GetZ();
+			data->h = (float)position[handle].GetH();
+			data->p = (float)position[handle].GetP();
+			data->r = (float)position[handle].GetR();
+			data->roadId = position[handle].GetTrackId();
+			data->laneId = position[handle].GetLaneId();
+			data->laneOffset = (float)position[handle].GetOffset();
+			data->s = (float)position[handle].GetS();
+		}
+
+		return 0;
+	}
+
+	RM_DLL_API int RM_GetSteeringTargetPosGlobal(int handle, float lookahead_distance, float * target_pos)
+	{
+		double pos_local[3], pos_global[3], angle, curvature;
+
+		if (odrManager == 0 || handle >= position.size())
+		{
+			return -1;
+		}
+
+		if (GetSteeringTarget(handle, lookahead_distance, pos_local, pos_global, &angle, &curvature) != 0)
+		{
+			return -1;
+		}
+
+		for (int i = 0; i < 3; i++) target_pos[i] = (float)pos_global[i];
+
+		return 0;
+	}
+
+	RM_DLL_API int RM_GetSteeringTargetPosLocal(int handle, float lookahead_distance, float * target_pos)
+	{
+		double pos_local[3], pos_global[3], angle, curvature;
+
+		if (odrManager == 0 || handle >= position.size())
+		{
+			return -1;
+		}
+
+		if (GetSteeringTarget(handle, lookahead_distance, pos_local, pos_global, &angle, &curvature) != 0)
+		{
+			return -1;
+		}
+
+		for (int i = 0; i < 3; i++) target_pos[i] = (float)pos_local[i];
+
+		return 0;
+	}
+
+	RM_DLL_API int RM_GetSteeringTargetAngle(int handle, float lookahead_distance, float * angle_f)
+	{
+		double pos_local[3], pos_global[3], angle, curvature;
+
+		if (odrManager == 0 || handle >= position.size())
+		{
+			return -1;
+		}
+
+		if (GetSteeringTarget(handle, lookahead_distance, pos_local, pos_global, &angle, &curvature) != 0)
+		{
+			return -1;
+		}
+
+		*angle_f = (float)angle;
+
+		return 0;
+	}
+
+	RM_DLL_API int RM_GetSteeringTargetCurvature(int handle, float lookahead_distance, float * curvature_f)
+	{
+		double pos_local[3], pos_global[3], angle, curvature;
+
+		if (odrManager == 0 || handle >= position.size())
+		{
+			return -1;
+		}
+
+		if (GetSteeringTarget(handle, lookahead_distance, pos_local, pos_global, &angle, &curvature) != 0)
+		{
+			return -1;
+		}
+
+		*curvature_f = (float)curvature;
+
 		return 0;
 	}
 
--- EnvironmentSimulator/RoadManagerDLL/roadmanagerdll.hpp
+++ EnvironmentSimulator/RoadManagerDLL/roadmanagerdll.hpp
@@ -18,6 +18,19 @@
   #define RM_DLL_API  // Leave empty on Mac
 #endif
 
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+	float h;
+	float p;
+	float r;
+	int   roadId;
+	int   laneId;
+	float laneOffset;
+	float s;
+} PositionData;
 
 #ifdef __cplusplus
 extern "C"
@@ -26,6 +39,135 @@ extern "C"
 
 	RM_DLL_API int RM_Init(const char *odrFilename);
 
+	RM_DLL_API int RM_Close();
+
+	/**
+	Create a position object
+	@return Handle to the position object, to use for operations
+	*/
+	RM_DLL_API int RM_CreatePosition();
+
+	/**
+	Get the total number fo roads in the road network of the currently loaded OpenDRIVE file.
+	@return Number of roads
+	*/
+	RM_DLL_API int RM_GetNumberOfRoads();
+
+	/**
+	Get the Road ID of the road with specified index. E.g. if there are 4 roads, index 3 means the last one.
+	@param index The index of the road
+	@return The ID of the road
+	*/
+	RM_DLL_API int RM_GetIdOfRoadFromIndex(int index);
+
+	/**
+	Get the lenght of road with specified ID
+	@param id The road ID 
+	@return The length of the road if ID exists, else 0.0
+	*/
+	RM_DLL_API float RM_GetRoadLength(int id);
+
+	/**
+	Get the number of drivable lanes of specified road
+	@param roadId The road ID
+	@param s The distance along the road at what point to check number of lanes (which can vary along the road)
+	@return The number of drivable lanes
+	*/
+	RM_DLL_API int RM_GetRoadNumberOfLanes(int roadId, float s);
+
+	/**
+	Get the ID of the lane given by index
+	@param roadId The road ID
+	@param laneIndex The index of the lane 
+	@param s The distance along the road at what point to look up the lane ID
+	@return The lane ID
+	*/
+	RM_DLL_API int RM_GetLaneIdByIndex(int roadId, int laneIndex, float s);
+
+	/**
+	Set position from road coordinates, world coordinates being calculated
+	@param handle Handle to the position object
+	@param roadId Road specifier
+	@param laneId Lane specifier
+	@param laneOffset Offset from lane center
+	@param s Distance along the specified road
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_SetLanePosition(int handle, int roadId, int laneId, int laneOffset, float s);
+
+	/**
+	Set s (distance) part of a lane position, world coordinates being calculated
+	@param handle Handle to the position object
+	@param s Distance along the specified road
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_SetS(int handle, float s);
+
+	/**
+	Set position from world coordinates, road coordinates being calculated
+	@param handle Handle to the position object
+	@param x cartesian coordinate x value
+	@param y cartesian coordinate y value
+	@param z cartesian coordinate z value
+	@param h rotation heading value
+	@param p rotation pitch value
+	@param r rotation roll value
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_SetWorldPosition(int handle, float x, float y, float z, float h, float p, float r);
+
+	/**
+	Move position forward along the road. Choose way randomly though any junctions.
+	@param handle Handle to the position object
+	@param ds distance (meter) to move
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_PositionMoveForward(int handle, float dist);
+
+	/**
+	Get the fields of the position of specified index
+	@param handle Handle to the position object
+	@param data Struct to fill in the values
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_GetPositionData(int handle, PositionData *data);
+
+	// Driver model functions
+	/**
+	Get the location, in global coordinate system, of the point at a specified distance from starting position along the road ahead
+	@param handle Handle to the position object from which to measure
+	@param lookahead_distance The distance, along the road, to the point
+	@param target_pos Array to fill in calculated X, Y and Z coordinate values
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_GetSteeringTargetPosGlobal(int handle, float lookahead_distance, float *target_pos);
+
+	/**
+	Get the location, in starting position local coordinate system, of the point at a specified distance along the road ahead
+	@param handle Handle to the position object from which to measure
+	@param lookahead_distance The distance, along the road, to the point
+	@param target_pos Array to fill in calculated X, Y and Z coordinate values
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_GetSteeringTargetPosLocal(int handle, float lookahead_distance, float *target_pos);
+
+	/**
+	Get the heading angle, in starting position local coordinate system, to the point at a specified distance along the road ahead
+	@param handle Handle to the position object from which to measure
+	@param lookahead_distance The distance, along the road, to the point
+	@param angle Pointer to variable where target angle will be written
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_GetSteeringTargetAngle(int handle, float lookahead_distance, float *angle);
+
+	/**
+	Get the curvature at the point at a specified distance along the road ahead
+	@param handle Handle to the position object from which to measure
+	@param lookahead_distance The distance, along the road, to the point
+	@param curvature Pointer to variable where target curvature will be written
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_GetSteeringTargetCurvature(int handle, float lookahead_distance, float *curvature);
 
 #ifdef __cplusplus
 }
