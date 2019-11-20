e05b1e68391682f618a3bbc20c45ee1668abc644
Emil Knabe <emil.knabe@volvocars.com>
2019-11-20 15:41:13 +0100

    Added release notes to binary and demo packages

--- generate_binary_dist.bat
+++ generate_binary_dist.bat
@@ -72,5 +72,6 @@ copy run\readme.txt %target_dir% /y
 
 copy 3rd_party_terms_and_licenses\* %target_dir%\3rd_party_terms_and_licenses /y
 copy LICENSE %target_dir% /y
+copy release_notes.txt %target_dir% /y
 
 copy buildVS15_64\EnvironmentSimulator\EgoSimulator\Release\EgoSimulator.exe %target_dir%\bin\win64\Release /y
