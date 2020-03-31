9c3c45992231bd081748be2b915bcb24de69f880
Emil Knabe <eknabe@volvocars.com>
2020-03-31 08:07:59 +0200

    Adding cut-in variant and cleaned cmake scripts
    This cut-in will set OverTaker relative speed in the Init section.
    The Brake maneuver will only have effect if the relative speed is non
    continuoes. Otherwise there will be a competition in speed setting.

--- create_VisualStudio2017_solution_winSDKv10_win32_for_adapt.bat
+++
@@ -1,4 +0,0 @@
-mkdir buildVS15_32_adapt
-cd buildVS15_32_adapt
-cmake -G "Visual Studio 15" -D USE_ENVSIM_ADAPT=True -D USE_OSG=True ..
-cd..
--- create_VisualStudio2017_solution_winSDKv10_win64_without_osg.bat
+++
@@ -1,4 +0,0 @@
-mkdir buildVS15_64_no_OSG
-cd buildVS15_64_no_OSG
-cmake -G "Visual Studio 15 Win64" -D USE_OSG=False ..
-cd..
