9814629c5abb65f5a4056f02820226c13ee975c5
Emil Knabe <emil.knabe@volvocars.com>
2020-06-25 17:45:17 +0200

    Scripts for building OSI stuff #skip_bins

--- scripts/generate_osi_libs.sh
+++ scripts/generate_osi_libs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # 
-# This script will build osi-visualizer for Windows from scratch 
-# as well as dependent software like protobuf and OSI classes
+# This script will build Protobuf and Open Simulation Interface (OSI) libraries  
+# needed for esmini to support OSI and output Ground Truth messages.
 # No system installations will be done (no admin rights required)
-# The result will be portable (dependent files copied into the install/release folder)
+#
+# The ambition is to support Windows, Linux and Mac. However the script is under
+# development and has not been tested as is on all three platforms.
 #
 # Prerequisites:
-# - Qt (https://www.qt.io/download)
 # - Git (with Bash) (https://git-scm.com/download/win)
 # - cmake (https://cmake.org/download/)
 # - Visual Studio (with C++ toolkit) (https://visualstudio.microsoft.com/downloads/)
@@ -16,7 +17,7 @@
 # - put this script in an empty folder
 # - open Git Bash in that folder 
 # - review and adjust system dependent parameters in section below
-# - run the script: ./setup-osi-visualizer.sh
+# - run the script: ./generate_osi_libs.sh
 # - wait (the build process will take approx. 15 minutes depending on...)
 # 
 # The folder osi-visualizer/build/Release (or Debug) will contain everything needed
@@ -26,21 +27,26 @@
 # -----------------------------------------------------------------------------------
 # Review and update settings in this section according to your system and preferences
 
-# Visual Studio 2019 - toolkit from Visual Studio 2017
-# GENERATOR=("Visual Studio 16 2019")
-# GENERATOR_ARGUMENTS="-T v141 -A x64"
+if [ "$OSTYPE" == "msys" ]; then
+	# Visual Studio 2019 - toolkit from Visual Studio 2017
+	GENERATOR=("Visual Studio 16 2019")
+	GENERATOR_TOOLSET="v141"
+	GENERATOR_ARGUMENTS="-A x64 -T ${GENERATOR_TOOLSET}"
 
-# Visual Studio 2019 - default toolkit
-# GENERATOR=("Visual Studio 16 2019")
-# GENERATOR_ARGUMENTS="-T v142 -A x64"
+	# Visual Studio 2019 - default toolkit
+	# GENERATOR=("Visual Studio 16 2019")
+	# GENERATOR_ARGUMENTS="-A x64 -T ${GENERATOR_TOOLSET}"
 
-# Visual Studio 2017 - default toolkit
-# GENERATOR=("Visual Studio 15 2017 Win64")
-# GENERATOR_ARGUMENTS="-T v141"
-
-# Unix Makefiles (for Ubuntu and other Linux systems)
-GENERATOR=("Unix Makefiles")
-GENERATOR_ARGUMENTS=""
+	# Visual Studio 2017 - default toolkit
+	# GENERATOR=("Visual Studio 15 2017 Win64")
+	# GENERATOR_ARGUMENTS="-T ${GENERATOR_TOOLSET}"
+elif [ "$OSTYPE" == "darwin"* ] || [ "$OSTYPE" == "linux-gnu"* ]; then
+	# Unix Makefiles (for Ubuntu and other Linux systems)
+	GENERATOR=("Unix Makefiles")
+	GENERATOR_ARGUMENTS=""
+else
+	echo Unknown OSTYPE: $OSTYPE
+fi
 
 
 PROTOBUF_VERSION=v3.5.0
@@ -72,7 +78,7 @@ then
 	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 		cmake -G "${GENERATOR[@]}" ${GENERATOR_ARGUMENTS} -D CMAKE_INSTALL_PREFIX=../install -DCMAKE_BUILD_TYPE=Debug .. -DCMAKE_C_FLAGS="-fPIC" 
 		cmake --build . --target install
-		mv ../install/lib/libz.a ../install/lib/libzd.a
+		mv ../install/lib/zlib.a ../install/lib/zlibd.a
 
 		rm CMakeCache.txt
 		cmake -G "${GENERATOR[@]}" ${GENERATOR_ARGUMENTS} -D CMAKE_INSTALL_PREFIX=../install -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_C_FLAGS="-fPIC" 
@@ -100,15 +106,15 @@ then
     cd protobuf
     git checkout v3.11.4
     mkdir build-code; cd build-code
-    if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
-        ZLIB_FILE_RELEASE=libz.a
-		ZLIB_FILE_DEBUG=libzd.a
-    elif [[ "$OSTYPE" == "win32" ]]; then
-        ZLIB_FILE_RELEASE=libz.lib
-		ZLIB_FILE_DEBUG=libzd.lib
+    if [ "$OSTYPE" == "linux-gnu"* ] || [ "$OSTYPE" == "darwin"* ]; then
+        ZLIB_FILE_RELEASE=zlib.a
+		ZLIB_FILE_DEBUG=zlibd.a
+    elif [[ "$OSTYPE" == "msys" ]]; then
+        ZLIB_FILE_RELEASE=zlib.lib
+		ZLIB_FILE_DEBUG=zlibd.lib
     fi
 
-    if [[ "$OSTYPE" != "darwin"* ]]; then
+    if [ "$OSTYPE" != "darwin"* ]; then
         cmake ../cmake -G "${GENERATOR[@]}" ${GENERATOR_ARGUMENTS} -DZLIB_LIBRARY=../../zlib-1.2.11/install/lib/$ZLIB_FILE_DEBUG -DZLIB_INCLUDE_DIR=../../zlib-1.2.11/install/include -DCMAKE_INSTALL_PREFIX=../protobuf-install -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_WITH_ZLIB=ON -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-fPIC" 
 
         cmake --build . --target install
@@ -154,19 +160,19 @@ then
 
     export PATH=$PATH:../../graphviz/release/bin:../../protobuf/protobuf-install/bin
 
-    if [[ "$OSTYPE" != "darwin"* ]]; then
+    if [ "$OSTYPE" != "darwin"* ]; then
         echo stop     
         cmake .. -G "${GENERATOR[@]}" ${GENERATOR_ARGUMENTS} -DCMAKE_INCLUDE_PATH=../protobuf/protobuf-install/include -DFILTER_PROTO2CPP_PY_PATH=../../proto2cpp -DINSTALL_LIB_DIR=$INSTALL_OSI_LIB_DIR/lib -DINSTALL_INCLUDE_DIR=$INSTALL_OSI_LIB_DIR/include -DCMAKE_INSTALL_PREFIX=$INSTALL_OSI_LIB_DIR -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_LIBRARY_PATH=../protobuf/protobuf-install/lib .. 
 
         # First bild OSI submodule separately since we need to rename the library before linking with the application
         cmake --build . --config Debug --target install
 
-       if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+       if [ "$OSTYPE" == "linux-gnu"* ]; then
    	    cp $INSTALL_OSI_LIB_DIR/lib/osi3/libopen_simulation_interface.so $INSTALL_OSI_LIB_DIR/lib/osi3/libopen_simulation_interfaced.so
 	    mv $INSTALL_OSI_LIB_DIR/lib/osi3/libopen_simulation_interface_pic.a $INSTALL_OSI_LIB_DIR/lib/osi3/libopen_simulation_interface_picd.a
 	    mv $INSTALL_OSI_LIB_DIR/lib/osi3/libopen_simulation_interface_static.a $INSTALL_OSI_LIB_DIR/lib/osi3/libopen_simulation_interface_staticd.a
 	    touch $INSTALL_OSI_LIB_DIR/lib/osi3/kalle.txt
-       elif [[ "$OSTYPE" == "win32" ]]; then 
+       elif [[ "$OSTYPE" == "msys" ]]; then 
 	    mv $INSTALL_OSI_LIB_DIR/lib/osi3/open_simulation_interface.dll $INSTALL_OSI_LIB_DIR/lib/osi3/open_simulation_interfaced.dll 
     	    mv $INSTALL_OSI_LIB_DIR/lib/osi3/open_simulation_interface_pic.lib $INSTALL_OSI_LIB_DIR/lib/osi3/open_simulation_interface_picd.lib 
 	    mv $INSTALL_OSI_LIB_DIR/lib/osi3/open_simulation_interface_static.lib $INSTALL_OSI_LIB_DIR/lib/osi3/open_simulation_interface_staticd.lib
