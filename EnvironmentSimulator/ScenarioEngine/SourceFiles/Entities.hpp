2987c25f985e463cd1d5cdd998a6a7e8ebda010c
ZijianHan-VolvoCars <zijian.han@volvocars.com>
2020-07-06 17:14:00 +0200

    Remove acceleration from object class, clean up for boundingbox

--- EnvironmentSimulator/ScenarioEngine/SourceFiles/Entities.hpp
+++ EnvironmentSimulator/ScenarioEngine/SourceFiles/Entities.hpp
@@ -68,7 +68,7 @@ namespace scenarioengine
 
 		Object(Type type) : type_(type), id_(0), trail_follow_index_(0), control_(Object::Control::INTERNAL),
 			speed_(0), wheel_angle_(0), wheel_rot_(0), route_(0), model_filepath_(""), ghost_(0), trail_follow_s_(0),
-		    odometer_(0),acceleration_(0) {}
+		    odometer_(0) {}
 		void SetControl(Control control) { control_ = control; }
 		Control GetControl() { return control_; }
 	};
