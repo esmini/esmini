4a13b1736d397a9fc4f1b91186358757c4285264
Melih Guldogus <melih.guldogus@volvocars.com>
2020-08-16 11:08:36 +0200

    EnvironmentSimulator/Unittest/RoadManager_test.cpp

--- EnvironmentSimulator/Unittest/RoadManager_test.cpp
+++ EnvironmentSimulator/Unittest/RoadManager_test.cpp
@@ -4,6 +4,89 @@
 #include <vector>
 #include <stdexcept>
 
+using namespace roadmanager;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class PolynomialTestFixture: public testing::Test
+{
+    public:
+        PolynomialTestFixture();
+        PolynomialTestFixture(double a, double b, double c, double d, double p_scale);
+        virtual ~PolynomialTestFixture();
+        //void SetUp() override;
+        //void TearDown() override;
+        //static void SetUpTestCase();
+        //static void TearDownTestCase();
+    protected:
+        Polynomial *poly;
+};
+
+PolynomialTestFixture::PolynomialTestFixture()
+{
+}
+
+PolynomialTestFixture::PolynomialTestFixture(double a, double b, double c, double d, double p_scale)
+{
+}
+
+PolynomialTestFixture::~PolynomialTestFixture()
+{
+}
+
+TEST_F(PolynomialTestFixture, TestConstructorEmpty)
+{
+    ASSERT_EQ(0, poly->GetA());
+    ASSERT_EQ(0, poly->GetB());
+    ASSERT_EQ(0, poly->GetC());
+    ASSERT_EQ(0, poly->GetD());
+    ASSERT_EQ(1, poly->GetPscale());
+}
+
+TEST_F(PolynomialTestFixture, TestConstructorArgument)
+{
+    Polynomial *poly = new Polynomial(1,-2,3,-4);
+    ASSERT_EQ(1, poly->GetA());
+    ASSERT_EQ(-2, poly->GetB());
+    ASSERT_EQ(3, poly->GetC());
+    ASSERT_EQ(-4, poly->GetD());
+    ASSERT_EQ(1, poly->GetPscale());
+    delete poly;
+}
+
+TEST_F(PolynomialTestFixture, TestConstructorArgumentPscale)
+{
+    Polynomial *poly = new Polynomial(1,-2,3,-4,2);
+    ASSERT_EQ(1, poly->GetA());
+    ASSERT_EQ(-2, poly->GetB());
+    ASSERT_EQ(3, poly->GetC());
+    ASSERT_EQ(-4, poly->GetD());
+    ASSERT_EQ(2, poly->GetPscale());
+    delete poly;
+}
+
+TEST_F(PolynomialTestFixture, TestSetGet)
+{
+    poly->Set(1,-2,3,-4);
+    ASSERT_EQ(1, poly->GetA());
+    ASSERT_EQ(-2, poly->GetB());
+    ASSERT_EQ(3, poly->GetC());
+    ASSERT_EQ(-4, poly->GetD());
+    ASSERT_EQ(1, poly->GetPscale());
+}
+
+TEST_F(PolynomialTestFixture, TestSetGetPscale)
+{
+    poly->Set(1,-2,3,-4, 2);
+    ASSERT_EQ(1, poly->GetA());
+    ASSERT_EQ(-2, poly->GetB());
+    ASSERT_EQ(3, poly->GetC());
+    ASSERT_EQ(-4, poly->GetD());
+    ASSERT_EQ(2, poly->GetPscale());
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 int main(int argc, char **argv)
 {
