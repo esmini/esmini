ced30ef6c4aa7dbe54939e41f023c9fb4981be7f
Melih Guldogus <melih.guldogus@volvocars.com>
2020-08-16 10:15:42 +0200

    layout for road manager related tests were added

---
+++ EnvironmentSimulator/Unittest/RoadManager_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "RoadManager.hpp"
+#include <vector>
+#include <stdexcept>
+
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
