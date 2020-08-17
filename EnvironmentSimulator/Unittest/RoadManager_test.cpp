9f9c95cfcf0a8422f5273262a8b295f5f0d4bd3e
Melih Guldogus <melih.guldogus@volvocars.com>
2020-08-17 16:00:31 +0200

    Polynomial class tests were added as full coverage test example for a given class

--- EnvironmentSimulator/Unittest/RoadManager_test.cpp
+++ EnvironmentSimulator/Unittest/RoadManager_test.cpp
@@ -6,7 +6,8 @@
 
 using namespace roadmanager;
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////// TESTS FOR CLASS -> Polynomial //////////
 
 class PolynomialTestFixture: public testing::Test
 {
@@ -14,13 +15,10 @@ class PolynomialTestFixture: public testing::Test
         PolynomialTestFixture();
         PolynomialTestFixture(double a, double b, double c, double d, double p_scale);
         virtual ~PolynomialTestFixture();
-        //void SetUp() override;
-        //void TearDown() override;
-        //static void SetUpTestCase();
-        //static void TearDownTestCase();
     protected:
         Polynomial poly;
 };
+
 
 PolynomialTestFixture::PolynomialTestFixture()
 {
@@ -95,7 +93,132 @@ TEST_F(PolynomialTestFixture, TestSetGetIndividual)
     ASSERT_EQ(-4, poly.GetD());
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+class PolynomialTestEvaluateEmptyParametrized: public testing::TestWithParam<std::tuple<double, double>>
+{
+    public:
+    protected:
+        Polynomial poly_empty;
+};
+
+TEST_P(PolynomialTestEvaluateEmptyParametrized, TestEvaluateEmptyConstructor)
+{
+    std::tuple<double, double> tuple = GetParam();
+    ASSERT_GE(std::get<0>(tuple), 0);
+    poly_empty.SetA(1);
+    poly_empty.SetB(-2);
+    poly_empty.SetC(3);
+    poly_empty.SetD(-4);
+    ASSERT_EQ(poly_empty.Evaluate(std::get<0>(tuple)), std::get<1>(tuple));
+}
+
+INSTANTIATE_TEST_CASE_P(TestEvaluateParamEmpty, PolynomialTestEvaluateEmptyParametrized, testing::Values(
+                                                std::make_tuple(2,-23),
+                                                std::make_tuple(0, 1)));
+
+class PolynomialTestEvaluateArgumentParametrized: public testing::TestWithParam<std::tuple<double, double>>
+{
+    public:
+    protected:
+        Polynomial poly_argument{1,-2,3,-4,2};
+};
+
+TEST_P(PolynomialTestEvaluateArgumentParametrized, TestEvaluateArgumentConstructor)
+{
+    std::tuple<double, double> tuple = GetParam();
+    ASSERT_GE(std::get<0>(tuple), 0);
+    ASSERT_EQ(poly_argument.Evaluate(std::get<0>(tuple)), std::get<1>(tuple));
+}
+
+INSTANTIATE_TEST_CASE_P(TestEvaluateParamArgument, PolynomialTestEvaluateArgumentParametrized, testing::Values(
+                                                std::make_tuple(2,-215),
+                                                std::make_tuple(0, 1)));
+
+
+class PolynomialTestEvaluatePrimEmptyParametrized: public testing::TestWithParam<std::tuple<double, double>>
+{
+    public:
+    protected:
+        Polynomial poly_empty;
+};
+
+TEST_P(PolynomialTestEvaluatePrimEmptyParametrized, TestEvaluatePrimEmptyConstructor)
+{
+    std::tuple<double, double> tuple = GetParam();
+    ASSERT_GE(std::get<0>(tuple), 0);
+    poly_empty.SetA(1);
+    poly_empty.SetB(-2);
+    poly_empty.SetC(3);
+    poly_empty.SetD(-4);
+    ASSERT_EQ(poly_empty.EvaluatePrim(std::get<0>(tuple)), std::get<1>(tuple));
+}
+
+INSTANTIATE_TEST_CASE_P(TestEvaluatePrimParamEmpty, PolynomialTestEvaluatePrimEmptyParametrized, testing::Values(
+                                                std::make_tuple(2,-38),
+                                                std::make_tuple(0, -2)));
+
+class PolynomialTestEvaluatePrimArgumentParametrized: public testing::TestWithParam<std::tuple<double, double>>
+{
+    public:
+    protected:
+        Polynomial poly_argument{1,-2,3,-4,2};
+};
+
+TEST_P(PolynomialTestEvaluatePrimArgumentParametrized, TestEvaluatePrimArgumentConstructor)
+{
+    std::tuple<double, double> tuple = GetParam();
+    ASSERT_GE(std::get<0>(tuple), 0);
+    ASSERT_EQ(poly_argument.EvaluatePrim(std::get<0>(tuple)), std::get<1>(tuple));
+}
+
+INSTANTIATE_TEST_CASE_P(TestEvaluatePrimParamArgument, PolynomialTestEvaluatePrimArgumentParametrized, testing::Values(
+                                                std::make_tuple(2,-170),
+                                                std::make_tuple(0, -2)));
+
+class PolynomialTestEvaluatePrimPrimEmptyParametrized: public testing::TestWithParam<std::tuple<double, double>>
+{
+    public:
+    protected:
+        Polynomial poly_empty;
+};
+
+TEST_P(PolynomialTestEvaluatePrimPrimEmptyParametrized, TestEvaluatePrimPrimEmptyConstructor)
+{
+    std::tuple<double, double> tuple = GetParam();
+    ASSERT_GE(std::get<0>(tuple), 0);
+    poly_empty.SetA(1);
+    poly_empty.SetB(-2);
+    poly_empty.SetC(3);
+    poly_empty.SetD(-4);
+    ASSERT_EQ(poly_empty.EvaluatePrimPrim(std::get<0>(tuple)), std::get<1>(tuple));
+}
+
+INSTANTIATE_TEST_CASE_P(TestEvaluatePrimPrimParamEmpty, PolynomialTestEvaluatePrimPrimEmptyParametrized, testing::Values(
+                                                std::make_tuple(2,-42),
+                                                std::make_tuple(0, 6)));
+
+class PolynomialTestEvaluatePrimPrimArgumentParametrized: public testing::TestWithParam<std::tuple<double, double>>
+{
+    public:
+    protected:
+        Polynomial poly_argument{1,-2,3,-4,2};
+};
+
+TEST_P(PolynomialTestEvaluatePrimPrimArgumentParametrized, TestEvaluatePrimPrimArgumentConstructor)
+{
+    std::tuple<double, double> tuple = GetParam();
+    ASSERT_GE(std::get<0>(tuple), 0);
+    ASSERT_EQ(poly_argument.EvaluatePrimPrim(std::get<0>(tuple)), std::get<1>(tuple));
+}
+
+INSTANTIATE_TEST_CASE_P(TestEvaluatePrimPrimParamArgument, PolynomialTestEvaluatePrimPrimArgumentParametrized, testing::Values(
+                                                std::make_tuple(2,-90),
+                                                std::make_tuple(0, 6)));
+
+//////////////////////////////////////////////////////////////////////
+
+////////// TESTS FOR CLASS -> OSIPoints //////////
+
+//////////////////////////////////////////////////////////////////////
 
 
 int main(int argc, char **argv)
