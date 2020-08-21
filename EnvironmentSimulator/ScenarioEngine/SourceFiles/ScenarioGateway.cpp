83b9f3e9a9165de17ade1ac93b5d46aaa4e049b5
Michael Eidland <michael.eidland@zenuity.com>
2020-08-21 12:53:39 +0200

    Add OSI yaw acceleration & renaming
    By taking the new yaw rate minus the old yaw rate
    and divid that with the timestep we get the yaw acceleraiton.
    
    Also renamed heading rate related variables to match the
    existing code naming convention.

--- EnvironmentSimulator/ScenarioEngine/SourceFiles/ScenarioGateway.cpp
+++ EnvironmentSimulator/ScenarioEngine/SourceFiles/ScenarioGateway.cpp
@@ -346,6 +346,7 @@ int ScenarioGateway::UpdateOSIMovingObject()
 		mobj_osi_internal.mobj[i]->mutable_base()->mutable_position()->set_z(objectState_[i]->state_.pos.GetZ());
 		mobj_osi_internal.mobj[i]->mutable_base()->mutable_orientation()->set_yaw(objectState_[i]->state_.pos.GetH());
 		mobj_osi_internal.mobj[i]->mutable_base()->mutable_orientation_rate()->set_yaw(objectState_[i]->state_.pos.GetHRate());
+		mobj_osi_internal.mobj[i]->mutable_base()->mutable_orientation_acceleration()->set_yaw(objectState_[i]->state_.pos.GetHAcc());
 		mobj_osi_internal.mobj[i]->mutable_base()->mutable_orientation()->set_pitch(objectState_[i]->state_.pos.GetP());
 		mobj_osi_internal.mobj[i]->mutable_base()->mutable_orientation()->set_roll(objectState_[i]->state_.pos.GetR());
 		mobj_osi_internal.mobj[i]->mutable_base()->mutable_velocity()->set_x(objectState_[i]->state_.pos.GetVelX());
