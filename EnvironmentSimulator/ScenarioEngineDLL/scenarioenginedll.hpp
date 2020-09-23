4b86b753098e2e2230ddebb4ee41c2f17b75535a
Emil Knabe <emil.knabe@volvocars.com>
2020-09-23 16:59:45 +0200

    Prepare release 1.7.7

--- EnvironmentSimulator/ScenarioEngineDLL/scenarioenginedll.hpp
+++ EnvironmentSimulator/ScenarioEngineDLL/scenarioenginedll.hpp
@@ -77,6 +77,10 @@ typedef struct
 extern "C"
 {
 #endif
+
+	// Basic interface
+	// 
+	
 	/**
 	Initialize the scenario engine
 	@param oscFilename Path to the OpenSCEANRIO file
@@ -103,14 +107,126 @@ extern "C"
 	SE_DLL_API int SE_Step();
 
 	/**
+	Stop simulation gracefully. Two purposes: 1. Release memory and 2. Prepare for next simulation, e.g. reset object lists.
+	*/
+	SE_DLL_API void SE_Close();
+
+	/**
+	Get simulation time in seconds
+	*/
+	SE_DLL_API float SE_GetSimulationTime();  // Get simulation time in seconds
+
+	/**
 	Get the bool value of the end of the scenario
 	*/
 	SE_DLL_API int SE_GetQuitFlag();
 
 	/**
-	Stop simulation gracefully. Two purposes: 1. Release memory and 2. Prepare for next simulation, e.g. reset object lists.
+	Report object position in cartesian coordinates
+	@param id Id of the object
+	@param timestamp Timestamp (not really used yet, OK to set 0)
+	@param x X coordinate
+	@param y Y coordinate
+	@param z Z coordinate
+	@param h Heading / yaw
+	@param p Pitch
+	@param r Roll
+	@param speed Speed in forward direction of the enitity
+	@return 0 if successful, -1 if not
 	*/
-	SE_DLL_API void SE_Close();
+	SE_DLL_API int SE_ReportObjectPos(int id, float timestamp, float x, float y, float z, float h, float p, float r, float speed);
+
+	/**
+	Report object position in road coordinates
+	@param id Id of the object
+	@param timestamp Timestamp (not really used yet, OK to set 0)
+	@param roadId Id of the road 
+	@param laneId Id of the lane 
+	@param laneOffset Lateral offset from center of specified lane
+	@param s Longitudinal distance of the position along the specified road
+	@param speed Speed in forward direction (s axis) of the enitity
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_ReportObjectRoadPos(int id, float timestamp, int roadId, int laneId, float laneOffset, float s, float speed);
+
+	/**
+	Get the number of entities in the current scenario
+	@return Number of entities
+	*/
+	SE_DLL_API int SE_GetNumberOfObjects();
+
+	/**
+	Get the state of specified object
+	@param index Index of the object. Note: not ID
+	@param state Pointer/reference to a SE_ScenarioObjectState struct to be filled in
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_GetObjectState(int index, SE_ScenarioObjectState* state);
+
+	/**
+	Get the state of specified object's ghost (special purpose lead vehicle)
+	@param index Index of the object. Note: not ID
+	@param state Pointer/reference to a SE_ScenarioObjectState struct to be filled in
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_GetObjectGhostState(int index, SE_ScenarioObjectState* state);
+
+	/**
+	Get information suitable for driver modeling of a point at a specified distance from object along the road ahead
+	@param object_id Handle to the position object from which to measure
+	@param lookahead_distance The distance, along the road, to the point
+	@param data Struct including all result values, see typedef for details
+	@param lookAheadMode Measurement strategy: Along 0=lane center, 1=road center (ref line) or 2=current lane offset. See roadmanager::Position::LookAheadMode enum
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_GetRoadInfoAtDistance(int object_id, float lookahead_distance, SE_RoadInfo* data, int lookAheadMode);
+
+	/**
+	Get information suitable for driver modeling of a ghost vehicle driving ahead of the ego vehicle
+	@param object_id Handle to the position object from which to measure (the actual externally controlled Ego vehicle, not ghost)
+	@param lookahead_distance The distance, along the ghost trail, to the point from the current Ego vehicle location
+	@param data Struct including all result values, see typedef for details
+	@param speed_ghost reference to a variable returning the speed that the ghost had at this point along trail
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_GetRoadInfoAlongGhostTrail(int object_id, float lookahead_distance, SE_RoadInfo* data, float* speed_ghost);
+
+	/**
+	Create an ideal object sensor and attach to specified vehicle
+	@param object_id Handle to the object to which the sensor should be attached
+	@param x Position x coordinate of the sensor in vehicle local coordinates
+	@param y Position y coordinate of the sensor in vehicle local coordinates
+	@param z Position z coordinate of the sensor in vehicle local coordinates
+	@param h heading of the sensor in vehicle local coordinates
+	@param fovH Horizontal field of view, in degrees
+	@param rangeNear Near value of the sensor depth range
+	@param rangeFar Far value of the sensor depth range
+	@param maxObj Maximum number of objects theat the sensor can track
+	@return Sensor ID (Global index of sensor), -1 if unsucessful
+	*/
+	SE_DLL_API int SE_AddObjectSensor(int object_id, float x, float y, float z, float h, float rangeNear, float rangeFar, float fovH, int maxObj);
+
+	/**
+	Fetch list of identified objects from a sensor
+	@param sensor_id Handle (index) to the sensor
+	@param list Array of object indices
+	@return Number of identified objects, i.e. length of list. -1 if unsuccesful.
+	*/
+	SE_DLL_API int SE_FetchSensorObjectList(int sensor_id, int* list);
+
+	/**
+	Register a function and optional parameter (ref) to be called back from esmini after each frame (update of scenario)
+	The current state of specified entity will be returned.
+	Complete or part of the state can then be overridden by calling the SE_ReportObjectPos/SE_ReportObjectRoadPos functions.
+	@param object_id Handle to the position object (entity)
+	@param SE_ScenarioObjectState A pointer to the function to be invoked
+	@param user_data Optional pointer to a local data object that will be passed as argument in the callback. Set 0/NULL if not needed.
+	*/
+	SE_DLL_API void SE_RegisterObjectCallback(int object_id, void (*fnPtr)(SE_ScenarioObjectState*, void*), void* user_data);
+
+
+	// OSI interface
+	// 
 
 	/**
 	Send OSI packages over UDP to specified IP address
@@ -127,18 +243,6 @@ extern "C"
 	*/
 	SE_DLL_API void SE_EnableOSIFile();
 
-	/**
-	Get simulation time in seconds
-	*/
-	SE_DLL_API float SE_GetSimulationTime();  // Get simulation time in seconds
-
-	SE_DLL_API int SE_ReportObjectPos(int id, float timestamp, float x, float y, float z, float h, float p, float r, float speed);
-	SE_DLL_API int SE_ReportObjectRoadPos(int id, float timestamp, int roadId, int laneId, float laneOffset, float s, float speed);
-
-	SE_DLL_API int SE_GetNumberOfObjects();
-
-	SE_DLL_API int SE_GetObjectState(int index, SE_ScenarioObjectState *state);
-	SE_DLL_API int SE_GetObjectGhostState(int index, SE_ScenarioObjectState *state);
 	/**
 	The SE_UpdateOSISensorView function returns a char array containing the osi SensorView serialized to a string
 	*/
@@ -186,53 +290,6 @@ extern "C"
 	*/
 	SE_DLL_API bool SE_OSIFileWrite();
 
-	/**
-	Create an ideal object sensor and attach to specified vehicle
-	@param object_id Handle to the object to which the sensor should be attached
-	@param x Position x coordinate of the sensor in vehicle local coordinates
-	@param y Position y coordinate of the sensor in vehicle local coordinates
-	@param z Position z coordinate of the sensor in vehicle local coordinates
-	@param h heading of the sensor in vehicle local coordinates
-	@param fovH Horizontal field of view, in degrees
-	@param rangeNear Near value of the sensor depth range
-	@param rangeFar Far value of the sensor depth range
-	@param maxObj Maximum number of objects theat the sensor can track
-	@return Sensor ID (Global index of sensor), -1 if unsucessful
-	*/
-	SE_DLL_API int SE_AddObjectSensor(int object_id, float x, float y, float z, float h, float rangeNear, float rangeFar, float fovH, int maxObj);
-
-	/**
-	Fetch list of identified objects from a sensor
-	@param sensor_id Handle (index) to the sensor
-	@param list Array of object indices
-	@return Number of identified objects, i.e. length of list. -1 if unsuccesful.
-	*/
-	SE_DLL_API int SE_FetchSensorObjectList(int sensor_id, int *list);
-
-	/**
-	Get information suitable for driver modeling of a point at a specified distance from object along the road ahead
-	@param object_id Handle to the position object from which to measure
-	@param lookahead_distance The distance, along the road, to the point
-	@param data Struct including all result values, see typedef for details
-	@param lookAheadMode Measurement strategy: Along 0=lane center, 1=road center (ref line) or 2=current lane offset. See roadmanager::Position::LookAheadMode enum
-	@return 0 if successful, -1 if not
-	*/
-	SE_DLL_API int SE_GetRoadInfoAtDistance(int object_id, float lookahead_distance, SE_RoadInfo *data, int lookAheadMode);
-
-	/**
-	Get information suitable for driver modeling of a ghost vehicle driving ahead of the ego vehicle
-	@param object_id Handle to the position object from which to measure (the actual externally controlled Ego vehicle, not ghost)
-	@param lookahead_distance The distance, along the ghost trail, to the point from the current Ego vehicle location
-	@param data Struct including all result values, see typedef for details
-	@param speed_ghost reference to a variable returning the speed that the ghost had at this point along trail
-	@return 0 if successful, -1 if not
-	*/
-	SE_DLL_API int SE_GetRoadInfoAlongGhostTrail(int object_id, float lookahead_distance, SE_RoadInfo *data, float *speed_ghost);
-
-	/**
-	*/
-//	SE_DLL_API void SE_RegisterObjectCallback(int object_id, void (*fnPtr)(SE_ScenarioObjectState*, void*));
-	SE_DLL_API void SE_RegisterObjectCallback(int object_id, void (*fnPtr)(SE_ScenarioObjectState*, void*), void *user_data);
 
 #ifdef __cplusplus
 }
