16af8eb26e459acb49d237b9689fdab07a427262
rjohan69 <rjohan69@volvocars.com>
2020-10-28 15:56:23 +0100

    added vehicle lane id to OSI ground truth

--- EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/OSIReporter.cpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/OSIReporter.cpp
@@ -495,6 +495,9 @@ int OSIReporter::UpdateOSIMovingObject(ObjectState* objectState)
 	obj_osi_internal.mobj->mutable_base()->mutable_acceleration()->set_y(objectState->state_.pos.GetAccY());
 	obj_osi_internal.mobj->mutable_base()->mutable_acceleration()->set_z(0);  // assume neglectable speed in z dimension
 
+	// Set OSI Moving Object Lane ID
+	obj_osi_internal.mobj->mutable_assigned_lane_id(objectState->state_.pos.GetLaneGlobalId());
+
 	return 0;
 }
 
