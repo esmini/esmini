411b6db61e5a99d3d34b71ef219fdb9bb34c565d
Emil Knabe <emil.knabe@volvocars.com>
2020-11-24 12:51:09 +0100

    Fix LaneChange relative lane bug #skip_bins
    In the case where the action object is on other side of reference lane
    compared to the referred relative target lane, the target lane
    calculation was wrong. Should be fixed with this commit.

--- EnvironmentSimulator/Modules/ScenarioEngine/OSCTypeDefs/OSCPrivateAction.cpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/OSCTypeDefs/OSCPrivateAction.cpp
@@ -232,7 +232,7 @@ void LatLaneChangeAction::Start()
 		// Find out target lane relative referred vehicle
 		target_lane_id_ = ((TargetRelative*)target_)->object_->pos_.GetLaneId() + target_->value_;
 		
-		if (target_lane_id_ == 0 || SIGN(object_->pos_.GetLaneId()) != SIGN(target_lane_id_))
+		if (target_lane_id_ == 0 || SIGN(((TargetRelative*)target_)->object_->pos_.GetLaneId()) != SIGN(target_lane_id_))
 		{
 			// Skip reference lane (id == 0)
 			target_lane_id_ = SIGN(target_lane_id_ - object_->pos_.GetLaneId()) * (abs(target_lane_id_) + 1);
