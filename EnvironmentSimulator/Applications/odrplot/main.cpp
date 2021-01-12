6f0da6b552f88f3c0b3c0a2ea056ba4e9f97fa72
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-01-12 09:39:19 +0100

    Variable naming style and better error handling

--- EnvironmentSimulator/Applications/odrplot/main.cpp
+++ EnvironmentSimulator/Applications/odrplot/main.cpp
@@ -29,36 +29,34 @@ using namespace roadmanager;
 int main(int argc, char *argv[])
 {
 	
-	std::string filename = "track.csv";
-	std::string samplingstep = "1.0";
+	std::string output_file_name = "track.csv";
+	std::ofstream file;
+	std::string sampling_step = "1.0";
 	double step_length_target;
 	
 	if (argc < 2)
 	{
-		printf("Usage: ordplot openDriveFile.xodr [Output file, default=output.csv] [samplingStep, default=1.0]\n");
+		printf("Usage: ordplot openDriveFile.xodr [Output file, default=output.csv] [Sampling_step, default=1.0]\n");
 		exit(0);
 	}else {
 		if (argc > 2){
-			filename =  argv[2];
+			output_file_name =  argv[2];
 		}
 		if (argc > 3){
-			samplingstep = argv[3];
-			step_length_target = std::stod(samplingstep);
+			sampling_step = argv[3];
+			step_length_target = std::stod(sampling_step);
 		}
 	}
 	
 	try
 	{
 		Position::LoadOpenDrive(argv[1]);
+		file.open(output_file_name);
 	}
 	catch (std::exception& e) 
 	{ 
 		LOG("exception: %s", e.what()); 
 	}
-	
-
-	std::ofstream file;
-	file.open(filename);
 
 	Position* pos = new Position();
 	
