1f7f35688a0ba3c0bb53bfdbbd6047be07569363
atingber <andreas.tingberg@volvocars.com>
2021-09-10 14:15:52 +0200

    Added hej

--- EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/ScenarioEngine.cpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/ScenarioEngine.cpp
@@ -917,6 +917,7 @@ void ScenarioEngine::ReplaceObjectInTrigger(Trigger* trigger, Object* obj1, Obje
 
 void ScenarioEngine::SetupGhost(Object* object)
 {
+	//Hej
 	// FollowGhostController special treatment:
 	// Create a new (ghost) vehicle and copy all actions from base object
 
