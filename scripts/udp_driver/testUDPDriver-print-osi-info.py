fc896eb4a52bd32049712f26fcafa95474ab9f5f
eknabevcc <emil.knabe@volvocars.com>
2022-11-11 07:50:07 +0100

    Show how to extract stationary objects #350

--- scripts/udp_driver/testUDPDriver-print-osi-info.py
+++ scripts/udp_driver/testUDPDriver-print-osi-info.py
@@ -8,11 +8,14 @@
 
    To run it:
    1. Open two terminals
-   2. From terminal 1, run: ./bin/esmini --window 60 60 800 400 --osc ./scripts/udp_driver/one_car_on_road.xosc --osi_receiver_ip 127.0.0.1
-   3. From terminal 2, run: ./scripts/udp_driver/testUDPDriver-print-osi-info.py
+   2. From terminal 1, run: ./scripts/udp_driver/testUDPDriver-print-osi-info.py
         or python ./scripts/udp_driver/testUDPDriver-print-osi-info.py
         or python3 ./scripts/udp_driver/testUDPDriver-print-osi-info.py
         depending on platform and file type associations
+   3. From terminal 2, run: ./bin/esmini --window 60 60 800 400 --osc ./scripts/udp_driver/one_car_on_road.xosc --osi_receiver_ip 127.0.0.1
+
+   Note: The example involves printing of some static data, e.g. stationary objects, that is only posted in the first OSI frame. Hence,
+         make sure to start the script BEFORE esmini, in order to not miss that very first OSI message.
 
    For complete driver control definitions, see esmini/EnvironmentSimulator/Modules/Controllers/ControllerUDPDriver.hpp
 '''
@@ -21,21 +24,29 @@ from udp_osi_common import *
 
 def print_osi_stuff(msg):
 
+    print("OSI message timestamp: {:.2f} seconds".format(msg.timestamp.seconds + msg.timestamp.nanos * 1e-9))
+
     # Print some static content typically only available in first message
-    for l in msg.lane:
+    print("{} lanes".format(len(msg.lane)))
+    for i, l in enumerate(msg.lane):
         clf = l.classification
-        print("lane {} type: {}".format(l.id.value, clf.type))
-        print("  centerline:")
+        print("  [{}] id {} type: {}".format(i, l.id.value, clf.type))
+        print("    centerline:")
         for c_line in clf.centerline:
             print("    x: {:.2f} y: {:.2f}".format(c_line.x, c_line.y))
 
+    print('{} stationary objects'.format(len(msg.stationary_object)))
+    for i, s in enumerate(msg.stationary_object):
+        print('  [{}] id {} type {}'.format(i, s.id.value, s.classification.type))
+        print('    pos.x {:.2f} pos.y {:.2f} rot.h {:.2f}'.format(s.base.position.x, s.base.position.y, s.base.orientation.yaw))
+
     # Print some dynamic content from the message
-    print('lane ids: {}'.format([l.id.value for l in msg.lane]))
+    print('{} moving objects'.format(len(msg.moving_object)))
     for i, o in enumerate(msg.moving_object):
-        print('Object[{}] id {}'.format(i, o.id.value))
-        print('   pos.x {:.2f} pos.y {:.2f} rot.h {:.2f}'.format(o.base.position.x, o.base.position.y, o.base.orientation.yaw))
-        print('   vel.x {:.2f} vel.y {:.2f} rot_rate.h {:.2f}'.format(o.base.velocity.x, o.base.velocity.y, o.base.orientation_rate.yaw))
-        print('   acc.x {:.2f} acc.y {:.2f} rot_acc.h {:.2f}'.format(o.base.acceleration.x, o.base.acceleration.y, o.base.orientation_acceleration.yaw))
+        print('  [{}] id {}'.format(i, o.id.value))
+        print('    pos.x {:.2f} pos.y {:.2f} rot.h {:.2f}'.format(o.base.position.x, o.base.position.y, o.base.orientation.yaw))
+        print('    vel.x {:.2f} vel.y {:.2f} rot_rate.h {:.2f}'.format(o.base.velocity.x, o.base.velocity.y, o.base.orientation_rate.yaw))
+        print('    acc.x {:.2f} acc.y {:.2f} rot_acc.h {:.2f}'.format(o.base.acceleration.x, o.base.acceleration.y, o.base.orientation_acceleration.yaw))
 
         lane_id = o.assigned_lane_id[0].value if len(msg.lane) > 0 and len(o.assigned_lane_id) > 0 else -1
         left_lane_id = -1
@@ -45,7 +56,7 @@ def print_osi_stuff(msg):
                 left_lane_id = l.classification.left_adjacent_lane_id[0].value if len(l.classification.left_adjacent_lane_id) > 0 else -1
                 right_lane_id = l.classification.right_adjacent_lane_id[0].value if len(l.classification.right_adjacent_lane_id) > 0 else -1
                 break
-        print('   lane id {} left adj lane id {} right adj lane id {}'.format(lane_id, left_lane_id, right_lane_id))
+        print('    lane id {} left adj lane id {} right adj lane id {}'.format(lane_id, left_lane_id, right_lane_id))
 
 
 if __name__ == "__main__":
@@ -70,7 +81,7 @@ if __name__ == "__main__":
         )
     )
 
-    while not done and counter < 20:
+    while not done and counter < 10:
         # Read OSI
         try:
             msg = osiReceiver.receive()
