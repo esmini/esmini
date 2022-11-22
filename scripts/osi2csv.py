4e114536701f53c3fa0fa979099d644f1a5891bf
eknabevcc <emil.knabe@volvocars.com>
2022-11-22 16:37:55 +0100

    Add acc and orientation vel/acc to osi2csv script

--- scripts/osi2csv.py
+++ scripts/osi2csv.py
@@ -25,13 +25,13 @@ class OSIFile():
             raise
 
         # write header
-        fcsv.write('time, id, name, x, y, z, vx, vy, vz, h, p, r, speed, wheel_angle, wheel_rot\n')
+        fcsv.write('time, id, name, x, y, z, vx, vy, vz, ax, ay, az, h, p, r, vh, vp, vr, ah, ap, ar, speed, wheel_angle, wheel_rot\n')
 
         # write data
         while self.read_next_message():
             t = self.osi_msg.timestamp.seconds + self.osi_msg.timestamp.nanos * 1e-9
             for o in self.osi_msg.moving_object:
-                fcsv.write('{:.6f}, {}, {}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}\n'.format(
+                fcsv.write('{:.6f}, {}, {}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}\n'.format(
                     t,
                     o.id.value,
                     'obj' + str(o.id.value),
@@ -41,9 +41,18 @@ class OSIFile():
                     o.base.velocity.x,
                     o.base.velocity.y,
                     o.base.velocity.z,
+                    o.base.acceleration.x,
+                    o.base.acceleration.y,
+                    o.base.acceleration.z,
                     o.base.orientation.yaw,
                     o.base.orientation.pitch,
                     o.base.orientation.roll,
+                    o.base.orientation_rate.yaw,
+                    o.base.orientation_rate.pitch,
+                    o.base.orientation_rate.roll,
+                    o.base.orientation_acceleration.yaw,
+                    o.base.orientation_acceleration.pitch,
+                    o.base.orientation_acceleration.roll,
                     math.sqrt(o.base.velocity.x**2 + o.base.velocity.x**2),
                     getattr(o.vehicle_attributes, 'wheel_data')[0].orientation.yaw,  # only one wheel for now
                     0.0  # wheel rotation not available
