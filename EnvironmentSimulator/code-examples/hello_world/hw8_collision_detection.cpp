c7d605754f8f189c3d5d79427c4db32741f0c26c
eknabevcc <emil.knabe@volvocars.com>
2023-01-08 12:58:55 +0100

    Add collision detection example to Hello World #368

---
+++ EnvironmentSimulator/code-examples/hello_world/hw8_collision_detection.cpp
@@ -0,0 +1,23 @@
+#include "esminiLib.hpp"
+#include "stdio.h"
+
+int main(int argc, char* argv[])
+{
+	SE_Init("../EnvironmentSimulator/Unittest/xosc/test-collision-detection.xosc", 1, 1, 0, 0);
+	SE_CollisionDetection(true);
+
+	for (int i = 0; i < 500; i++)
+	{
+		SE_Step();
+
+		for (int j = 0; j < SE_GetNumberOfObjects(); j++)
+		{
+			for (int k = 0; k < SE_GetObjectNumberOfCollisions(j); k++)
+			{
+				printf("Collision[%d]: %d\n", j, SE_GetObjectCollision(j, k));
+			}
+		}
+	}
+
+	return 0;
+}
