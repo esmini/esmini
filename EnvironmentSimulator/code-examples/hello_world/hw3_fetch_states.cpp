0e4564eb52570c433404735b06091dd1f821dfae
eknabevcc <emil.knabe@volvocars.com>
2023-02-25 15:51:41 +0100

    Check return code

--- EnvironmentSimulator/code-examples/hello_world/hw3_fetch_states.cpp
+++ EnvironmentSimulator/code-examples/hello_world/hw3_fetch_states.cpp
@@ -6,7 +6,11 @@ int main(int argc, char* argv[])
 	(void)argc;
 	(void)argv;
 
-	SE_Init("../resources/xosc/cut-in.xosc", 0, 1, 0, 0);
+	if (SE_Init("../resources/xosc/cut-in.xosc", 0, 1, 0, 0) != 0)
+	{
+		printf("Failed to initialize scenario\n");
+		return -1;
+	}
 
 	for (int i = 0; i < 500; i++)
 	{
