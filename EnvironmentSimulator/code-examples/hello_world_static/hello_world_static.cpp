b86330a1c2f7568bb5b4fda196473b2ba7e62670
eknabevcc <emil.knabe@volvocars.com>
2023-03-28 12:39:45 +0200

    Add static variant of esminiLib

---
+++ EnvironmentSimulator/code-examples/hello_world_static/hello_world_static.cpp
@@ -0,0 +1,22 @@
+#include "esminiLib.hpp"
+
+int main(int argc, char* argv[])
+{
+    if (argc > 1)
+    {
+        SE_InitWithArgs(argc, const_cast<const char**>(argv));
+    }
+    else
+    {
+        SE_Init("../resources/xosc/cut-in.xosc", 0, 1, 0, 0);
+    }
+
+    for (int i = 0; i < 500; i++)
+    {
+        SE_Step();
+    }
+
+    SE_Close();
+
+    return 0;
+}
