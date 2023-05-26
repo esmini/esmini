170e106c1fd66c28a93798f28bda4091c6921ea5
eknabevcc <emil.knabe@volvocars.com>
2023-05-26 07:46:08 +0200

    Demonstrate position type conversion (#437)

--- EnvironmentSimulator/code-examples/hello_world/hw4_external_ego_control.cpp
+++ EnvironmentSimulator/code-examples/hello_world/hw4_external_ego_control.cpp
@@ -4,14 +4,22 @@
 int main(int argc, char* argv[])
 {
     const char* filename = argc > 1 ? argv[1] : "../resources/xosc/cut-in_external.xosc";
+    SE_ScenarioObjectState state;
 
-    SE_Init(filename, 0, 1, 0, 0);
+    if (SE_Init(filename, 0, 1, 0, 0) != 0)
+    {
+        return -1;
+    }
 
     for (int i = 0; i < 500 && SE_GetQuitFlag() != 1; i++)
     {
         SE_ReportObjectPos(SE_GetId(0), 0.0f, 8.0f, static_cast<float>(i), 10.0f, float(1.57 + 0.01 * i), 0.0f, 0.0f);
 
         SE_Step();
+
+        // fetch position in terms of road coordinates
+        SE_GetObjectState(SE_GetId(0), &state);
+        printf("road_id: %d s: %.3f lane_id %d lane_offset: %.3f\n", state.roadId, state.s, state.laneId, state.laneOffset);
 
         if (i == 100)
         {
