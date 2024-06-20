cd1e57d42ac26989b3c7d5bac56dc7fda8751ec1
rizwan <mrizwans@volvocars.com>
2024-06-20 12:32:55 +0200

    Added Exported Macro In Controller

--- EnvironmentSimulator/Modules/Controllers/Controller.hpp
+++ EnvironmentSimulator/Modules/Controllers/Controller.hpp
@@ -4,6 +4,33 @@
 #include <any>
 #include <map>
 
+// Define EXPORTED for any platform
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef WIN_EXPORT
+    // Exporting...
+    #ifdef __GNUC__
+      #define EXPORTED __attribute__ ((dllexport))
+    #else
+      #define EXPORTED __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #else
+    #ifdef __GNUC__
+      #define EXPORTED __attribute__ ((dllimport))
+    #else
+      #define EXPORTED __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #endif
+  #define NOT_EXPORTED
+#else
+  #if __GNUC__ >= 4
+    #define EXPORTED __attribute__ ((visibility ("default")))
+    #define NOT_EXPORTED  __attribute__ ((visibility ("hidden")))
+  #else
+    #define EXPORTED
+    #define NOT_EXPORTED
+  #endif
+#endif
+
 
 namespace scenarioengine::controller
 {
