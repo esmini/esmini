41003bb447bdbb38f2c2905e2b7c2f2de17d1feb
climent jackey clement <cclement@volvocars.com>
2024-09-25 12:18:39 +0200

    updates xosc and xodr files according to scheama

---
+++ scripts/run_scenrio_comply.py
@@ -0,0 +1,148 @@
+import xml.etree.ElementTree as ET
+import xmlschema
+from lxml import etree
+import argparse
+import os
+import sys
+from pathlib import Path
+
+SCHEMA_MAPPINGS = {
+    'xosc': {
+        '0': 'OpenSCENARIOv1.0.xsd',
+        '1': 'OpenSCENARIOv1.1.1.xsd',
+        '2': 'OpenSCENARIOv1.2.xsd',
+        '3': 'OpenSCENARIOv1.3.xsd'
+    },
+    'xodr': {
+        '4': 'OpenDRIVE_1.4H.xsd',
+        '5': 'OpenDRIVE_1.5.xsd',
+        '6': 'OpenDRIVE_1.6/opendrive_16_core.xsd',
+        '7': 'OpenDRIVE_1.7/localSchema/opendrive_17_core.xsd',
+        '8': 'OpenDRIVE_1.8/local_schema/OpenDRIVE_Core.xsd',
+    }
+}
+
+class XmlValidation:
+    def __init__(self):
+        self.use_xsd11 = False
+        self.xml_file_names = []
+        self.xsd_files_path = r"resources/schema/"
+        self.errors = []
+
+    def is_xml11_needed(self):
+        return self.use_xsd11
+
+    def set_xml11_needed(self):
+        self.use_xsd11 = True
+
+    def get_xml_files_to_validate(self):
+        return self.xml_file_names
+
+    def set_xml_files(self, file_name):
+        self.xml_file_names.append(file_name)
+
+    def get_xsd_files_path(self):
+        return self.xsd_files_path
+
+    def get_xml_type(self, file_path):
+        return 'xodr' if file_path.endswith('.xodr') else  'xosc'
+
+    def print_errors(self):
+        for item in self.errors:
+            print(item)
+
+    def get_xml_header_minor_revision(self, file_path):
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        if self.get_xml_type(file_path) == 'xosc':
+            header = root.findall('./FileHeader')
+        else:
+            header = root.findall('./header')
+        revMinor = header[0].attrib['revMinor']
+        return revMinor
+
+    def get_xsd_to_validate(self, revMinor, type_):
+        if type_ in SCHEMA_MAPPINGS and revMinor in SCHEMA_MAPPINGS[type_]:
+            return os.path.join(self.xsd_files_path, SCHEMA_MAPPINGS[type_][revMinor])
+        else:
+            print(f"Schema for type {type_} and revMinor {revMinor} not found")
+            return None
+
+    def validate(self, xml_file, schema_file):
+        if schema_file is None:
+            print(f"Error: Schema file is not provided for {xml_file}")
+            return
+        try:
+            # Create the XMLSchema object once outside the loop
+            my_schema = xmlschema.XMLSchema11(schema_file) if self.is_xml11_needed() else xmlschema.XMLSchema(schema_file)
+
+            # Parse the XML document only once
+            parser = etree.XMLParser(recover=False)
+            document_tree = etree.parse(xml_file, parser)
+
+            # Iterate over errors and print them directly, avoiding an intermediate list
+            for error in my_schema.iter_errors(document_tree):
+                elem = error.elem
+                line_number = elem.sourceline if elem is not None else "unknown"
+                element_name = elem.tag if elem is not None else "unknown"
+                self.errors.append(f"{xml_file}:{line_number}: Schemas validity error : element '{element_name}' : {error.reason}")
+
+            # Print a summary message after all errors are processed
+            if self.errors:
+                print(f"{xml_file} fails to validates. Found {len(self.errors)} error(s):")
+            else:
+                print(f"{xml_file} validates.")
+
+        except xmlschema.validators.exceptions.XMLSchemaValidationError as e:
+            print(f"An error occurred during validation: {e}")
+        except etree.XMLSyntaxError as e:
+            print(f"XML Parsing Error: {e}")
+
+    def validate_argument(self, paths):
+        for path in paths:
+            path = Path(path)
+            # Check if it's a valid file or directory
+            if path.is_file():
+                # Validate the file extension
+                if path.suffix in ['.xosc', '.xodr']:
+                    return
+                else:
+                    raise argparse.ArgumentTypeError(f"File '{path}' has an unsupported extension. Only .xosc and .xodr are allowed.")
+            elif path.is_dir():
+                return
+            else:
+                raise argparse.ArgumentTypeError(f"'{path}' is neither a valid file nor a directory.")
+
+    def set_xml_files_to_validate(self, file_paths):
+        for path in file_paths:
+            path = Path(path)
+            if os.path.isfile(path):
+                file_path = os.path.join(os.getcwd() + '/' + str(path))
+                if "/test/" in file_path:
+                    continue
+                self.set_xml_files(file_path)
+            # Check if the path is a directory and walk through its files
+            elif os.path.isdir(path):
+                for root, dirs, files in os.walk(path):
+                    if "/test/" in root:
+                        continue
+                    for file in files:
+                        # Only consider .xosc or .xodr files
+                        if file.endswith(('.xosc', '.xodr')):
+                            file_path = os.path.join(root, file)
+                            self.set_xml_files(file_path)
+
+    def main(self, arg):
+        self.validate_argument(arg)
+        self.set_xml_files_to_validate(arg)
+        for file in self.get_xml_files_to_validate():
+            revMinor = self.get_xml_header_minor_revision(file)
+            self.validate(file, self.get_xsd_to_validate(revMinor, self.get_xml_type(file)))
+        self.print_errors()
+
+if __name__ == "__main__":
+    validator = XmlValidation()
+    if len(sys.argv) == 1:
+        validator.main([os.getcwd()])
+    else:
+        validator.main(sys.argv[1:])
