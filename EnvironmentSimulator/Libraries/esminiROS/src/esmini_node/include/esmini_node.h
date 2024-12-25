36d24541fce8026a9668aca1ffb7a5c64c7746e9
Spiraline <spiraline@rideflux.com>
2024-12-25 16:01:18 +0900

    [ros1] Publish object states msg

--- EnvironmentSimulator/Libraries/esminiROS/src/esmini_node/include/esmini_node.h
+++ EnvironmentSimulator/Libraries/esminiROS/src/esmini_node/include/esmini_node.h
@@ -1,5 +1,8 @@
 #include <ros/ros.h>
 #include <ros/package.h>
+#include "esmini/ObjectState.h"
+#include "esmini/ObjectStates.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "esminiLib.hpp"
 
 class ESMiniNode
@@ -7,6 +10,10 @@ class ESMiniNode
 private:
     ros::NodeHandle nh_;
     ros::Timer      scenario_timer_;
+
+    ros::Publisher object_states_pub_;
+
+    esmini::ObjectStates object_states_msg_;
 
 public:
     ESMiniNode(int argc, char* argv[]);
