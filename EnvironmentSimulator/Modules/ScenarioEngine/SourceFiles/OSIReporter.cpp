a481483c619eed93399e26680d735ed1990ac893
slundel6 <slundel6@volvocars.com>
2025-02-05 16:39:08 +0100

    load static data on request, serialize only on UDP or logfile

--- EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/OSIReporter.cpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/OSIReporter.cpp
@@ -364,8 +364,11 @@ int OSIReporter::UpdateOSIGroundTruth(const std::vector<std::unique_ptr<ObjectSt
     {
         osi_static_gt_loaded_ = SetOSIStaticExternalData();
         // Clear the static data now when it has been reported once
-        obj_osi_external.gt->SerializeToString(&osiGroundTruth.ground_truth);
-        osiGroundTruth.size = static_cast<unsigned int>(obj_osi_external.gt->ByteSizeLong());
+        if (GetUDPClientStatus() == 0 || IsFileOpen())
+        {
+            obj_osi_external.gt->SerializeToString(&osiGroundTruth.ground_truth);
+            osiGroundTruth.size = static_cast<unsigned int>(obj_osi_external.gt->ByteSizeLong());
+        }
     }
 
     if (GetUDPClientStatus() == 0)
