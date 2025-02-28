202be60279cc3044c66dcc2fb14c9c7635ea38ef
climent jackey clement <cclement@volvocars.com>
2025-02-28 16:00:33 +0100

    enabled all dat to csv tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -61,65 +61,65 @@ else
     echo "Unsupported OS: " $OSTYPE
 fi
 
-# if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "linux-gnu"* ]]; then
+if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
-#     cd $UNIT_TEST_FOLDER
+    cd $UNIT_TEST_FOLDER
 
-#     echo $'\n'Run unit tests:
+    echo $'\n'Run unit tests:
 
-#     echo $'\n'OperatingSystem_test:
-#     if ! ${EXE_FOLDER}/OperatingSystem_test --disable_stdout; then
-#         exit_with_msg "OperatingSystem_test failed"
-#     fi
+    echo $'\n'OperatingSystem_test:
+    if ! ${EXE_FOLDER}/OperatingSystem_test --disable_stdout; then
+        exit_with_msg "OperatingSystem_test failed"
+    fi
 
-#     echo $'\n'CommonMini_test:
-#     if ! ${EXE_FOLDER}/CommonMini_test --disable_stdout; then
-#         exit_with_msg "CommonMini_test failed"
-#     fi
+    echo $'\n'CommonMini_test:
+    if ! ${EXE_FOLDER}/CommonMini_test --disable_stdout; then
+        exit_with_msg "CommonMini_test failed"
+    fi
 
-#     echo $'\n'RoadManager_test:
-#     if ! ${EXE_FOLDER}/RoadManager_test --disable_stdout; then
-#         exit_with_msg "RoadManager_test failed"
-#     fi
+    echo $'\n'RoadManager_test:
+    if ! ${EXE_FOLDER}/RoadManager_test --disable_stdout; then
+        exit_with_msg "RoadManager_test failed"
+    fi
 
-#     echo $'\n'ScenarioPlayer_test:
-#     if ! ${EXE_FOLDER}/ScenarioPlayer_test --disable_stdout; then
-#         exit_with_msg "ScenarioPlayer_test failed"
-#     fi
+    echo $'\n'ScenarioPlayer_test:
+    if ! ${EXE_FOLDER}/ScenarioPlayer_test --disable_stdout; then
+        exit_with_msg "ScenarioPlayer_test failed"
+    fi
 
-#     echo $'\n'ScenarioEngineDll_test:
-#     if ! ${EXE_FOLDER}/ScenarioEngineDll_test --disable_stdout; then
-#         exit_with_msg "ScenarioEngineDll_test failed"
-#     fi
+    echo $'\n'ScenarioEngineDll_test:
+    if ! ${EXE_FOLDER}/ScenarioEngineDll_test --disable_stdout; then
+        exit_with_msg "ScenarioEngineDll_test failed"
+    fi
 
-#     echo $'\n'ScenarioEngine_test:
-#     if ! ${EXE_FOLDER}/ScenarioEngine_test --disable_stdout; then
-#         exit_with_msg "ScenarioEngine_test failed"
-#     fi
+    echo $'\n'ScenarioEngine_test:
+    if ! ${EXE_FOLDER}/ScenarioEngine_test --disable_stdout; then
+        exit_with_msg "ScenarioEngine_test failed"
+    fi
 
-#     ls -al *.tga *.ppm
+    ls -al *.tga *.ppm
 
-#     echo $'\n'RoadManagerDll_test:
-#     if ! ${EXE_FOLDER}/RoadManagerDll_test --disable_stdout; then
-#         exit_with_msg "RoadManagerDll_test failed"
-#     fi
+    echo $'\n'RoadManagerDll_test:
+    if ! ${EXE_FOLDER}/RoadManagerDll_test --disable_stdout; then
+        exit_with_msg "RoadManagerDll_test failed"
+    fi
 
-#     echo $'\n'FollowRoute_test:
-#     if ! ${EXE_FOLDER}/FollowRoute_test --disable_stdout; then
-#         exit_with_msg "FollowRoute_test failed"
-#     fi
+    echo $'\n'FollowRoute_test:
+    if ! ${EXE_FOLDER}/FollowRoute_test --disable_stdout; then
+        exit_with_msg "FollowRoute_test failed"
+    fi
 
-#     echo $'\n'FollowRouteController_test:
-#     if ! ${EXE_FOLDER}/FollowRouteController_test --disable_stdout; then
-#         exit_with_msg "FollowRouteController_test failed"
-#     fi
+    echo $'\n'FollowRouteController_test:
+    if ! ${EXE_FOLDER}/FollowRouteController_test --disable_stdout; then
+        exit_with_msg "FollowRouteController_test failed"
+    fi
 
-#     echo $'\n'Replayer_test:
-#     if ! ${EXE_FOLDER}/Replayer_test; then
-#         exit_with_msg "Replayer_test failed"
-#     fi
+    echo $'\n'Replayer_test:
+    if ! ${EXE_FOLDER}/Replayer_test; then
+        exit_with_msg "Replayer_test failed"
+    fi
 
-# fi
+fi
 
 cd $SMOKE_TEST_FOLDER
 
@@ -133,14 +133,14 @@ if [[ "$skipOpenGLTests" == false ]]; then
 
 fi
 
-# echo $'\n'Run ALKS test suite:
+echo $'\n'Run ALKS test suite:
 
-# if ! ${PYTHON} alks_suite.py -t $timeout; then
-#     exit_with_msg "alks_suite test failed"
-# fi
+if ! ${PYTHON} alks_suite.py -t $timeout; then
+    exit_with_msg "alks_suite test failed"
+fi
 
-# echo $'\n'Run NCAP test suite:
+echo $'\n'Run NCAP test suite:
 
-# if ! ${PYTHON} ncap_suite.py -t $timeout; then
-#     exit_with_msg "ncap_suite test failed"
-# fi
+if ! ${PYTHON} ncap_suite.py -t $timeout; then
+    exit_with_msg "ncap_suite test failed"
+fi
