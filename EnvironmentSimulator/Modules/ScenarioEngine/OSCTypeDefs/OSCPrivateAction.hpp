8894de9d73e979b6e88720d7fc51b46afefc3f3c
slundel6 <slundel6@volvocars.com>
2025-04-17 10:17:16 +0200

    State machine. Only take init heading into account

--- EnvironmentSimulator/Modules/ScenarioEngine/OSCTypeDefs/OSCPrivateAction.hpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/OSCTypeDefs/OSCPrivateAction.hpp
@@ -528,6 +528,13 @@ namespace scenarioengine
     public:
         typedef enum
         {
+            INIT,
+            MOVE_RIGID,
+            MOVE_DYNAMIC
+        } MoveState;
+
+        typedef enum
+        {
             DISTANCE,
         } DistType;
 
@@ -599,7 +606,8 @@ namespace scenarioengine
 
     private:
         double acceleration_;
-        double   prev_lateral_distance_ = 0.0;
+        MoveState move_state_;
+        double init_heading_;
         bool latched_to_target_ = false;
     };
 
