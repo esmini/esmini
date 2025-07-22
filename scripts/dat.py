928c726810dd59c4eab8a5203b9f1757d6e175dc
slundel6 <slundel6@volvocars.com>
2025-07-22 15:14:12 +0200

    Can create a csv almost identical to original

--- scripts/dat.py
+++ scripts/dat.py
@@ -2,9 +2,12 @@ import argparse
 import struct
 import os
 import enum
+from collections import defaultdict
 
 VERSION_MAJOR = 1
 VERSION_MINOR = 0
+        
+EPSILON = 0.0001
 
 class PacketId(enum.Enum):
     """Enum for packet IDs."""
@@ -84,41 +87,41 @@ def read_dat_header(file):
     model_filename = read_string_packet(file)
     return (version_major, version_minor, odr_filename, model_filename)
 
-ObjectStateStructDat = {
-    "id": {},
-    "model_id": {},
-    "obj_type": {},
-    "obj_category": {},
-    "ctrl_type": {},
-    "time": {},
-    "name": "",
-    "speed": {},
-    "wheel_angle": {},
-    "wheel_rot": {},
-    "centerOffsetX": {},
-    "centerOffsetY": {},
-    "centerOffsetZ": {},
-    "width": {},
-    "length": {},
-    "height": {},
-    "scaleMode": {},
-    "visibilityMask": {},
-    "active": {},
-    "x": {},
-    "y": {},
-    "z": {},
-    "h": {},
-    "p": {},
-    "r": {},
-    "roadId": {},
-    "laneId": {},
-    "offset": {},
-    "t": {},
-    "s": {}
-}
 
 class DATFile():
-    def __init__(self, filename):
+    ObjectStateStructDat = {
+        "id": None,
+        "model_id": None,
+        "obj_type": None,
+        "obj_category": None,
+        "ctrl_type": None,
+        "time": None,
+        "name": "",
+        "speed": None,
+        "wheel_angle": None,
+        "wheel_rot": None,
+        "centerOffsetX": None,
+        "centerOffsetY": None,
+        "centerOffsetZ": None,
+        "width": None,
+        "length": None,
+        "height": None,
+        "scaleMode": None,
+        "visibilityMask": None,
+        "active": None,
+        "x": None,
+        "y": None,
+        "z": None,
+        "h": None,
+        "p": None,
+        "r": None,
+        "roadId": None,
+        "laneId": None,
+        "offset": None,
+        "t": None,
+        "s": None
+    }
+    def __init__(self, filename, extended=False):
         if not os.path.isfile(filename):
             print('ERROR: dat-file not found: {}'.format(filename))
             return
@@ -137,128 +140,164 @@ class DATFile():
             )
             exit(-1)
 
+        self.extended = extended
+
         self.filename = filename
         self.file_size = os.path.getsize(filename)
 
-        self.labels = [key for key in ObjectStateStructDat.items()]
+        self.labels = self.ObjectStateStructDat.keys()
+        self.object_ids = set()
+        self.object_events_map = defaultdict(list)
+        self.object_state_cache = defaultdict()
+        self.min_timestep = float('inf')
+        self.timestamp = 0.0
+        self.end_time = 0.0
+        self.id_to_search_idx = {}
+
         self.data = []
 
+        self.parse_data()
+
+        self.build_data()
+
+    def build_data(self):
+        self.object_state_cache.clear()
+
+
+        dt = self.min_timestep
+        # TODO: Allow custom argument for dt
+        start_time = 0.0
+        for _, events in self.object_events_map.items():
+            start_time = min(start_time, events[0]["time"])
+        
+        for obj_id in self.object_ids:
+            self.id_to_search_idx[obj_id] = 0
+        
+        template = self.ObjectStateStructDat
+        if self.extended:
+            labels = self.get_labels_line_extended()
+        else:
+            labels = self.get_labels_line()
+        
+        t = start_time
+        while t <= self.end_time:
+            for obj_id in self.object_ids:
+                events = self.object_events_map.get(obj_id, [])
+                last_state = self.object_state_cache.get(obj_id, template.copy())
+
+                if events:
+                    start_idx = self.id_to_search_idx.get(obj_id, 0)
+                    for i in range(start_idx, len(events)):
+                        if events[i]["time"] <= t + EPSILON:
+                            last_state = events[i]
+                            self.id_to_search_idx[obj_id] = i
+                        else:
+                            break
+
+                state = last_state.copy()
+                state["time"] = t
+                if state["active"]:
+                    self.data.append([state[label] for label in labels])
+            t += dt
+
+    def parse_data(self):
         while self.file.tell() < self.file_size:
             p_id = read_packet_header(self.file)
             if p_id == PacketId.TIMESTAMP.value:
-                print(f"Timestamp: {read_dtype(self.file, DataType.double)}")
+                self.timestamp = read_dtype(self.file, DataType.double)
+
             elif p_id == PacketId.OBJ_ID.value:
+                if self.ObjectStateStructDat["id"] in self.object_ids:
+                    self.object_events_map[self.ObjectStateStructDat["id"]].append(self.ObjectStateStructDat.copy())
+                    self.object_state_cache[self.ObjectStateStructDat["id"]] = self.ObjectStateStructDat.copy()
+
                 obj_id = read_dtype(self.file, DataType.int32)
-                print(f"Object ID: {obj_id}")
+                self.ObjectStateStructDat["id"] = obj_id
+
+                if obj_id in self.object_state_cache:
+                    self.ObjectStateStructDat = self.object_state_cache[obj_id]
+                    
+                    if self.min_timestep == float('inf'):
+                        self.min_timestep = self.timestamp - self.ObjectStateStructDat["time"]
+                    else:
+                        dt = self.timestamp - self.ObjectStateStructDat["time"]
+                        if not (abs(dt) < 0.001):
+                            self.min_timestep = min(self.min_timestep, dt)
+                
+                self.ObjectStateStructDat["time"] = self.timestamp
+                self.ObjectStateStructDat["active"] = True
+                self.ObjectStateStructDat["id"] = obj_id
+                self.object_ids.add(obj_id)
+                
             elif p_id == PacketId.MODEL_ID.value:
                 model_id = read_dtype(self.file, DataType.int32)
-                print(f"Model ID: {model_id}")
+                self.ObjectStateStructDat["model_id"] = model_id
             elif p_id == PacketId.OBJ_TYPE.value:
                 obj_type = read_dtype(self.file, DataType.int32)
-                print(f"Object Type: {obj_type}")
+                self.ObjectStateStructDat["obj_type"] = obj_type
             elif p_id == PacketId.OBJ_CATEGORY.value:
                 obj_category = read_dtype(self.file, DataType.int32)
-                print(f"Object Category: {obj_category}")
+                self.ObjectStateStructDat["obj_category"] = obj_category
             elif p_id == PacketId.CTRL_TYPE.value:
                 ctrl_type = read_dtype(self.file, DataType.int32)
-                print(f"Control Type: {ctrl_type}")
+                self.ObjectStateStructDat["ctrl_type"] = ctrl_type
             elif p_id == PacketId.NAME.value:
                 name = read_string_packet(self.file)
-                print(f"Name: {name}")
+                self.ObjectStateStructDat["name"] = name
             elif p_id == PacketId.SPEED.value:
                 speed = read_dtype(self.file, DataType.double)
-                print(f"Speed: {speed}")
+                self.ObjectStateStructDat["speed"] = speed
             elif p_id == PacketId.WHEEL_ANGLE.value:
                 wheel_angle = read_dtype(self.file, DataType.double)
-                print(f"Wheel Angle: {wheel_angle}")
+                self.ObjectStateStructDat["wheel_angle"] = wheel_angle
             elif p_id == PacketId.WHEEL_ROT.value:
                 wheel_rot = read_dtype(self.file, DataType.double)
-                print(f"Wheel Rotation: {wheel_rot}")
+                self.ObjectStateStructDat["wheel_rot"] = wheel_rot
             elif p_id == PacketId.POSE.value:
-                x = read_dtype(self.file, DataType.double)
-                y = read_dtype(self.file, DataType.double)
-                z = read_dtype(self.file, DataType.double)
-                h = read_dtype(self.file, DataType.double)
-                p = read_dtype(self.file, DataType.double)
-                r = read_dtype(self.file, DataType.double)
-                print(f"Pose: x={x}, y={y}, z={z}, h={h}, p={p}, r={r}")
+                self.ObjectStateStructDat["x"] = read_dtype(self.file, DataType.double)
+                self.ObjectStateStructDat["y"] = read_dtype(self.file, DataType.double)
+                self.ObjectStateStructDat["z"] = read_dtype(self.file, DataType.double)
+                self.ObjectStateStructDat["h"] = read_dtype(self.file, DataType.double)
+                self.ObjectStateStructDat["p"] = read_dtype(self.file, DataType.double)
+                self.ObjectStateStructDat["r"] = read_dtype(self.file, DataType.double)
             elif p_id == PacketId.BOUNDING_BOX.value:
-                centerOffsetX = read_dtype(self.file, DataType.double)
-                centerOffsetY = read_dtype(self.file, DataType.double)
-                centerOffsetZ = read_dtype(self.file, DataType.double)
-                width = read_dtype(self.file, DataType.double)
-                length = read_dtype(self.file, DataType.double)
-                height = read_dtype(self.file, DataType.double)
-                print(f"Bounding Box: centerOffsetX={centerOffsetX}, centerOffsetY={centerOffsetY}, centerOffsetZ={centerOffsetZ}, width={width}, length={length}, height={height}")
+                self.ObjectStateStructDat["centerOffsetX"] = read_dtype(self.file, DataType.double)
+                self.ObjectStateStructDat["centerOffsetY"] = read_dtype(self.file, DataType.double)
+                self.ObjectStateStructDat["centerOffsetZ"] = read_dtype(self.file, DataType.double)
+                self.ObjectStateStructDat["width"] = read_dtype(self.file, DataType.double)
+                self.ObjectStateStructDat["length"] = read_dtype(self.file, DataType.double)
+                self.ObjectStateStructDat["height"] = read_dtype(self.file, DataType.double)
             elif p_id == PacketId.SCALE_MODE.value:
-                scale_mode = read_dtype(self.file, DataType.int32)
-                print(f"Scale Mode: {scale_mode}")
+                self.ObjectStateStructDat["scaleMode"] = read_dtype(self.file, DataType.int32)
             elif p_id == PacketId.VISIBILITY_MASK.value:
-                visibility_mask = read_dtype(self.file, DataType.int32)
-                print(f"Visibility Mask: {visibility_mask}")
+                self.ObjectStateStructDat["visibilityMask"] = read_dtype(self.file, DataType.int32)
             elif p_id == PacketId.ROAD_ID.value:
-                road_id = read_dtype(self.file, DataType.uint32)
-                print(f"Road ID: {road_id}")
+                self.ObjectStateStructDat["roadId"] = read_dtype(self.file, DataType.uint32)
             elif p_id == PacketId.LANE_ID.value:
-                lane_id = read_dtype(self.file, DataType.int32)
-                print(f"Lane ID: {lane_id}")
+                self.ObjectStateStructDat["laneId"] = read_dtype(self.file, DataType.int32)
             elif p_id == PacketId.POS_OFFSET.value:
-                offset = read_dtype(self.file, DataType.double)
-                print(f"Position Offset: {offset}")
+                self.ObjectStateStructDat["offset"] = read_dtype(self.file, DataType.double)
             elif p_id == PacketId.POS_T.value:
-                t = read_dtype(self.file, DataType.double)
-                print(f"Position T: {t}")
+                self.ObjectStateStructDat["t"] = read_dtype(self.file, DataType.double)
             elif p_id == PacketId.POS_S.value:
-                s = read_dtype(self.file, DataType.double)
-                print(f"Position S: {s}")
+                self.ObjectStateStructDat["s"] = read_dtype(self.file, DataType.double)
             elif p_id == PacketId.OBJ_DELETED.value:
-                print(f"Object Deleted:")
+                self.ObjectStateStructDat["active"] = False
+            elif p_id == PacketId.END_OF_SCENARIO.value:
+                self.end_time = read_dtype(self.file, DataType.double)
 
     def get_header_line(self):
         return f'Version: {self.version_major}.{self.version_minor}, OpenDRIVE: {self.odr_filename}, 3DModel: {self.model_filename}'
 
     def get_labels_line(self):
-        return 'time, id, name, x, y, z, h, p, r, speed, wheel_angle, wheel_rot'
+        return ['time', 'id', 'name', 'x', 'y', 'z', 'h', 'p', 'r', 'speed', 'wheel_angle', 'wheel_rot']
 
     def get_data_line(self, data):
-        return '{:.3f}, {}, {}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}'.format(
-                data.time,
-                data.id,
-                data.name.decode('utf-8'),
-                data.x,
-                data.y,
-                data.z,
-                data.h,
-                data.p,
-                data.r,
-                data.speed,
-                data.wheel_angle,
-                data.wheel_rot
-            )
+        """ Will contain extended data if self.extended is True """
+        return ', '.join(f"{x:.3f}" if isinstance(x, float) else str(x) for x in data)
 
     def get_labels_line_extended(self):
-        return 'time, id, name, x, y, z, h, p, r, roadId, laneId, offset, t, s, speed, wheel_angle, wheel_rot'
-
-    def get_data_line_extended(self, data):
-        return '{:.3f}, {}, {}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {}, {}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}'.format(
-                data.time,
-                data.id,
-                data.name.decode('utf-8'),
-                data.x,
-                data.y,
-                data.z,
-                data.h,
-                data.p,
-                data.r,
-                data.roadId,
-                data.laneId,
-                data.offset,
-                data.t,
-                data.s,
-                data.speed,
-                data.wheel_angle,
-                data.wheel_rot
-            )
+        return ['time', 'id', 'name', 'x', 'y', 'z', 'h', 'p', 'r', 'roadId', 'laneId', 'offset', 't', 's', 'speed', 'wheel_angle', 'wheel_rot']
 
     def get_labels_line_array(self):
         return [
@@ -294,55 +333,55 @@ class DATFile():
 
     def get_data_line_array(self, data):
         return [
-            data.time,
-            data.id,
-            data.name.decode('utf-8'),
-            data.model_id,
-            data.obj_type,
-            data.obj_category,
-            data.ctrl_type,
-            data.x,
-            data.y,
-            data.z,
-            data.h,
-            data.p,
-            data.r,
-            data.speed,
-            data.wheel_angle,
-            data.wheel_rot,
-            data.centerOffsetX,
-            data.centerOffsetY,
-            data.centerOffsetZ,
-            data.width,
-            data.length,
-            data.height,
-            data.scaleMode,
-            data.visibilityMask,
-            data.roadId,
-            data.laneId,
-            data.offset,
-            data.t,
-            data.s];
+            data["time"],
+            data["id"],
+            data["name"].decode('utf-8'),
+            data["modelId"],
+            data["objType"],
+            data["objCategory"],
+            data["ctrlType"],
+            data["x"],
+            data["y"],
+            data["z"],
+            data["h"],
+            data["p"],
+            data["r"],
+            data["speed"],
+            data["wheel_angle"],
+            data["wheel_rot"],
+            data["centerOffsetX"],
+            data["centerOffsetY"],
+            data["centerOffsetZ"],
+            data["width"],
+            data["length"],
+            data["height"],
+            data["scaleMode"],
+            data["visibilityMask"],
+            data["roadId"],
+            data["laneId"],
+            data["offset"],
+            data["t"],
+            data["s"]];
 
+    # TODO
+    # def print_csv(self, extended = False, include_file_refs = True):
 
-    def print_csv(self, extended = False, include_file_refs = True):
+    #     # Print header
+    #     if include_file_refs:
+    #         print(self.get_header_line())
 
-        # Print header
-        if include_file_refs:
-            print(self.get_header_line())
+    #     # Print column headings / value types
+    #     if extended:
+    #         print(self.get_labels_line_extended())
+    #     else:
+    #         print(self.get_labels_line())
 
-        # Print column headings / value types
-        if extended:
-            print(self.get_labels_line_extended())
-        else:
-            print(self.get_labels_line())
-
-        # Read and print all rows of data
-        for data in self.data:
-            if extended:
-                print(self.get_data_line_extended(data))
-            else:
-                print(self.get_data_line(data))
+    #     # Read and print all rows of data
+    #     for data in self.data:
+    #         if extended:
+    #             print(self.get_data_line_extended(data))
+    #         else:
+    #             print(self.get_data_line(data))
 
     def save_csv(self, extended = False, include_file_refs = True):
         csvfile = os.path.splitext(self.filename)[0] + '.csv'
@@ -358,32 +397,30 @@ class DATFile():
 
         # Save column headings / value types
         if extended:
-            fcsv.write(self.get_labels_line_extended() + '\n')
+            fcsv.write(', '.join(self.get_labels_line_extended()) + '\n')
         else:
-            fcsv.write(self.get_labels_line() + '\n')
+            fcsv.write(', '.join(self.get_labels_line()) + '\n')
 
         # Read and save all rows of data
         for data in self.data:
-            if extended:
-                fcsv.write(self.get_data_line_extended(data) + '\n')
-            else:
-                fcsv.write(self.get_data_line(data) + '\n')
+            fcsv.write(self.get_data_line(data) + '\n')
 
         fcsv.close()
 
-    def save_dat(self, filename):
-        try:
-            fdat = open(filename, 'wb')
-        except OSError:
-            print('ERROR: Could not open file {} for writing'.format(filename))
-            raise
+    # TODO
+    # def save_dat(self, filename):
+    #     try:
+    #         fdat = open(filename, 'wb')
+    #     except OSError:
+    #         print('ERROR: Could not open file {} for writing'.format(filename))
+    #         raise
 
-        fdat.write(self.header)
+    #     fdat.write(self.header)
 
-        for d in self.data:
-            fdat.write(d)
+    #     for d in self.data:
+    #         fdat.write(d)
 
-        fdat.close()
+    #     fdat.close()
 
     def close(self):
         self.file.close()
@@ -400,6 +437,6 @@ if __name__ == "__main__":
     # Execute the parse_args() method
     args = parser.parse_args()
 
-    dat = DATFile(args.filename)
-    dat.print_csv(args.extended, args.file_refs)
+    dat = DATFile(args.filename, args.extended)
+    dat.print_csv(args.file_refs)
     dat.close()
