6dc203ab670e21b350257e72c3c1315a8b7523ef
slundel6 <slundel6@volvocars.com>
2025-07-25 12:53:58 +0200

    All tests passing

--- EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/ScenarioGateway.cpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/ScenarioGateway.cpp
@@ -1119,8 +1119,13 @@ void ScenarioGateway::WriteStatesToFile(const double simulation_time)
 {
     if (dat_logger_.IsWriteFileOpen())
     {
-        dat_logger_.SetSimulationTime(simulation_time);
-        dat_logger_.WriteToDat(objectState_);
+        // Write only if simulation time is greater than last timestep, ensuring time strictly increasing (ghost might restart)
+        if (simulation_time <= 0.0 + SMALL_NUMBER || simulation_time > last_timestamp_ + SMALL_NUMBER)
+        {
+            dat_logger_.SetSimulationTime(simulation_time);
+            dat_logger_.WriteToDat(objectState_);
+        }
+        last_timestamp_ = std::max(last_timestamp_, simulation_time);
     }
 }
 
