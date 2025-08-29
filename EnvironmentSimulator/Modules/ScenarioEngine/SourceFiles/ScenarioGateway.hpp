2bbbba6f27a2a38203a744a1ea6ad51bf7301e9f
slundel6 <slundel6@volvocars.com>
2025-08-29 19:54:46 +0200

    Store dt if it changes

--- EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/ScenarioGateway.hpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/ScenarioGateway.hpp
@@ -371,7 +371,7 @@ namespace scenarioengine
         }
         ObjectState *getObjectStatePtrById(int id);
         int          getObjectStateById(int id, ObjectState &objectState) const;
-        void         WriteStatesToFile(const double simulation_time);
+        void         WriteStatesToFile(const double simulation_time, const double dt);
         int          RecordToFile(std::string filename, std::string odr_filename, std::string model_filename);
 
         std::vector<std::unique_ptr<ObjectState>> objectState_;
