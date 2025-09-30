c714d56476e02754e5ab316a3516d3f6f25cf7c8
eknabevcc <emil.knabe@volvocars.com>
2025-09-30 10:00:41 +0200

    Add support for TrafficLight 3D-models

--- EnvironmentSimulator/Applications/esmini/main.cpp
+++ EnvironmentSimulator/Applications/esmini/main.cpp
@@ -98,6 +98,26 @@ static int execute_scenario(int argc, char* argv[])
 
         retval = player->Frame(dt);
 
+#if _USE_OSG
+#if 1  // traffic light visualization test
+        unsigned int period = 500;
+        if (player->viewer_ != nullptr && player->viewer_->roadGeom != nullptr)
+        {
+            for (auto& pair : player->viewer_->roadGeom->traffic_light_red_yellow_green_)
+            {
+                for (unsigned int i = 0; i < 3; i++)
+                {
+                    if (static_cast<unsigned int>(player->GetCounter()) % period == i * period / 3)
+                    {
+                        pair.second.SetState(i, !pair.second.GetState(i));
+                        printf("Traffic light id %u light %u state %d\n", pair.first, i, pair.second.GetState(i));
+                    }
+                }
+            }
+        }
+#endif
+#endif  // _USE_OSG
+
 #ifdef _USE_IMPLOT
         if (plot != nullptr && plot->IsModeSynchronuous())
         {
